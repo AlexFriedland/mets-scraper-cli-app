b9720c51dd65d72a49e53d7c23d706ca6e0f2ce4
Alexander Friedland <friedlandaj@gmail.com>
2017-06-08 01:52:45 +0000

    Added version control

---
+++ lib/mets-scraper/version.rb
@@ -0,0 +1,4 @@
+
+module MetsScraper
+  VERSION = "0.1.0"
+end
--- lib/mets_scraper.rb
+++ lib/mets_scraper.rb
@@ -1,8 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require 'mets-scraper/version' #from avi codealong
 
 require_relative "./mets-scraper/cli"
 require_relative "./mets-scraper/player"
 require_relative "./mets-scraper/position"
 require_relative "./mets-scraper/scraper"
+
+module MetsScraper
+  #
+end
