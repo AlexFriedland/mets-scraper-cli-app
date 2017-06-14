55331fdef793c6868a1c9d293719caa61b18bc81
Alexander Friedland <friedlandaj@gmail.com>
2017-06-14 01:02:11 +0000

    player initialize

--- lib/mets-scraper/player.rb
+++ lib/mets-scraper/player.rb
@@ -1,4 +1,15 @@
 class MetsScraper::Player
 
   attr_accessor :name, :number, :bt, :height, :weight, :DOB
+
+  @@all = []
+
+  def initialize(name, number, bt, height, weight, DOB)
+    @name = name
+    @number = number
+    @bt = bt
+    @height = height
+    @weight = weight
+    @dob = dob
+  end
 end
