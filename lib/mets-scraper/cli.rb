b69e570715f6a5e4ca8f8b566c57e4f87d7613d3
Alexander Friedland <friedlandaj@gmail.com>
2017-06-16 00:36:20 +0000

    tweaked cli

--- lib/mets-scraper/cli.rb
+++ lib/mets-scraper/cli.rb
@@ -5,97 +5,44 @@ class MetsScraper::CLI
   #stub of our interface
 
   def call
-    player_roster
+    menu
     #MetsScraper.get_page
     #MetsScraper.make_list
-    menu
   end
 
   def player_roster
-    puts "Player Roster of the New York Metropolitans:"
-
-
-    puts <<-DOC.gsub /^\*/, ''
-    PITCHERS
-NO.	NAME	POS	BAT	THW	AGE	HT	WT	BIRTH PLACE	SALARY
-39	Jerry Blevins	RP	L	L	33	6-6	190	Johnson City, TN	N/A
-48	Jacob deGrom	SP	L	R	28	6-4	180	Deland, FL	N/A
-66	Josh Edgin	RP	R	L	30	6-1	246	Lewiston, PA	N/A
-65	Robert Gsellman	SP	R	R	23	6-4	200	Santa Monica, CA	N/A
-33	Matt Harvey	SP	R	R	28	6-4	217	New London, CT	N/A
-56	Tyler Pill	SP	L	R	27	6-1	205	San Dimas, CA	N/A
-55	Neil Ramirez	RP	R	R	28	6-4	215	Virginia Beach, VA	N/A
-43	Addison Reed	RP	L	R	28	6-4	230	Montclair, CA	N/A
-59	Fernando Salas	RP	R	R	32	6-2	200	Huatabampo, Mexico	N/A
-51	Paul Sewald	RP	R	R	27	6-2	195	Las Vegas, NV	N/A
-49	Josh Smoker	RP	L	L	28	6-2	195	Calhoun, GA	N/A
-45	Zack Wheeler	SP	L	R	27	6-4	196	Smyrna, GA	N/A
-
-CATCHERS
-NO.	NAME	POS	BAT	THW	AGE	HT	WT	BIRTH PLACE	SALARY
-18	Travis d'Arnaud	C	R	R	28	6-2	211	Long Beach, CA	N/A
-44	Rene Rivera	C	R	R	33	5-10	215	Bayamon, Puerto Rico	N/A
-
-INFIELDERS
-NO.	NAME	POS	BAT	THW	AGE	HT	WT	BIRTH PLACE	SALARY
-13	Asdrubal Cabrera	SS	B	R	31	6-0	205	Puerto La Cruz, Venezuela	N/A
-21	Lucas Duda	1B	L	R	31	6-4	256	Riverside, CA	N/A
-4	Wilmer Flores	3B	R	R	25	6-3	203	Valencia, Venezuela	N/A
-7	Jose Reyes	3B	B	R	33	6-0	195	Villa Gonzalez, Dominican Republic	N/A
-15	Matt Reynolds	SS	R	R	26	6-1	205	Tulsa, OK	N/A
-54	T.J. Rivera	1B	R	R	28	6-1	205	Bronx, NY	N/A
-20	Neil Walker	2B	B	R	31	6-3	210	Pittsburgh, PA	N/A
-
-OUTFIELDERS
-NO.	NAME	POS	BAT	THW	AGE	HT	WT	BIRTH PLACE	SALARY
-
-19	Jay Bruce	RF	L	L	30	6-3	227	Beaumont, TX	N/A
-30	Michael Conforto	LF	L	R	24	6-1	215	Seattle, WA	N/A
-3	  Curtis Granderson	CF	L	R	36	6-1	199	Blue Island, IL	N/A
-12	Juan Lagares	CF	R	R	28	6-1	215	Constanza, Dominican Republic	N/A
-
-DISABLED LIST
-
-NO.	NAME	           POS	BAT	THW	AGE	HT	WT	BIRTH PLACE	SALARY
-
-52	Yoenis Cespedes  DL10	LF	R	R	31	5-10	220	Campechuela, Cuba	N/A
-27	Jeurys Familia   DL60	RP	R	R	27	6-3	242	Santo Domingo, Dominican Republic	N/A
-67	Seth Lugo        DL60	RP	R	R	27	6-4	205	Shreveport, LA	N/A
-32	Steven Matz      DL10	SP	R	L	26	6-2	201	Stony Brook, NY	N/A
-29	Tommy Milone     DL10	SP	L	L	30	6-0	215	Grenada Hills, CA	N/A
-34	Noah Syndergaard DL60	SP	L	R	24	6-6	242	Mansfield, TX	N/A
-5	  David Wright     DL60	3B	R	R	34	6-0	206	Norfolk, VA	N/A
-
-MINORS
-NO.	NAME	           POS	BAT	THW	AGE	HT	WT	BIRTH PLACE	SALARY
---	Wuilmer Becerra	 RF	R	R	22	6-3	225	Caracas, Venezuela	N/A
---	Gavin Cecchini	 SS	R	R	23	6-2	200	Lake Charles, LA	N/A
---	Chris Flexen	   RP	R	R	22	6-3	230	Newark, CA	N/A
---	Sean Gilmartin	 RP	L	L	27	6-2	205	Thousand Oaks, CA	N/A
---	Erik Goeddel	   RP	R	R	28	6-3	185	San Mateo, CA	N/A
---	Marcos Molina	   SP	R	R	22	6-3	188	Santiago, Dominican Republic	N/A
---	Rafael Montero	 RP	R	R	26	6-0	200	Higuerito, Dominican Republic	N/A
---	Tomas Nido	     C	R	R	23	6-0	211	San Juan, FL	N/A
---	Brandon Nimmo	   LF	L	R	24	6-3	206	Cheyenne, WY	N/A
---	Kevin Plawecki	 C	R	R	26	6-2	224	Carmel, IN	N/A
---	Hansel Robles	   RP	R	R	26	5-11	185	Bonao, Dominican Republic	N/A
---	Amed Rosario	   SS	R	R	21	6-2	190	Santo Domingo Centro, Dominican Republic	N/A
-    DOC
   end
 
   def menu
-    puts "Press 1 to view the roster by players, press 2 to view by position. Type 'exit' to quit:"
+    puts "Welcome to the player roster of the New York Metropolitans.
+
+    Which position would you like to view?
+
+    1. Pitchers
+    2. Catchers
+    3. Infield
+    4. Outfield
+    5. Full Roster
+
+    Type 'exit' to quit"
+
 
     input = gets.strip
 
     if input == "1"
-      puts "ROSTER BY PLAYER"
+      puts "PITCHERS"
       player_roster #by player
       menu
     elsif input == "2"
-      puts "ROSTER BY POSITION"
+      puts "CATCHERS"
       player_roster #by position
       menu
+    elsif input == "3"
+      puts "INFIELD:"
+    elsif input == "4"
+      puts "OUTFIELD:"
+    elsif input == "5"
+      puts "FULL ROSTER"
     elsif input == "exit"
       abort("See you soon!")
     else
@@ -103,7 +50,45 @@ NO.	NAME	           POS	BAT	THW	AGE	HT	WT	BIRTH PLACE	SALARY
       menu
     end
   end
-
-
-
 end
+
+
+=begin
+
+puts <<-DOC.gsub /^\*/, ''
+Pitchers
+#	 	Name	B/T	Ht	Wt	DOB
+39		Jerry Blevins	L/L	6'6"	190lbs	9/6/83
+48		Jacob deGrom	L/R	6'4"	180lbs	6/19/88
+66		Josh Edgin	R/L	6'1"	245lbs	12/17/86
+65		Robert Gsellman	R/R	6'4"	205lbs	7/18/93
+33		Matt Harvey	R/R	6'4"	215lbs	3/27/89
+67		Seth Lugo	R/R	6'4"	225lbs	11/17/89
+32		Steven Matz	R/L	6'2"	200lbs	5/29/91
+50		Rafael Montero	R/R	6'0"	185lbs	10/17/90
+55		Neil Ramirez	R/R	6'4"	215lbs	5/25/89
+43		Addison Reed	L/R	6'4"	230lbs	12/27/88
+59		Fernando Salas	R/R	6'2"	200lbs	5/30/85
+51		Paul Sewald	R/R	6'3"	205lbs	5/26/90
+45		Zack Wheeler	L/R	6'4"	195lbs	5/30/90
+Catchers
+#	 	Name	B/T	Ht	Wt	DOB
+18		Travis d'Arnaud	R/R	6'2"	210lbs	2/10/89
+44		Rene Rivera	R/R	5'10"	215lbs	7/31/83
+Infield
+#	 	Name	B/T	Ht	Wt	DOB
+2		Gavin Cecchini	R/R	6'2"	200lbs	12/22/93
+21	Lucas Duda	L/R	6'4"	255lbs	2/3/86
+4		Wilmer Flores	R/R	6'3"	205lbs	8/6/91
+7		Jose Reyes	S/R	6'0"	195lbs	6/11/83
+54	T.J. Rivera	R/R	6'1"	205lbs	10/27/88
+Outfield
+#	 	Name	B/T	Ht	Wt	DOB
+19	Jay Bruce	L/L	6'3"	225lbs	4/3/87
+52	Yoenis Cespedes	R/R	5'10"	220lbs	10/18/85
+30	Michael Conforto	L/R	6'1"	215lbs	3/1/93
+3		Curtis Granderson	L/R	6'1"	200lbs	3/16/81
+12	Juan Lagares	R/R	6'1"	215lbs	3/17/89
+DOC
+
+=end
